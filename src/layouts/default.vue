6a51ffead00dfc3a83a56ceb672fd90905f14c0f
Celina Ferreira Ribeiro <ribeiro.f.celina@gmail.com>
2024-02-12 10:37:04 -0300

    links

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,12 +1,68 @@
 <template>
-  <v-app>
-    <DefaultBar />
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
 
-    <DefaultView />
+         <v-list>
+          <v-list-item
+            prepend-avatar="https://img.freepik.com/fotos-premium/avatar-mulher-geek-furiosa_949597-34.jpg?w=740"
+            title="Maria Silva"
+            subtitle="maria@google.com"
+          >
+            <template v-slot:append>
+              <v-btn
+                size="small"
+                variant="text"
+                icon="mdi-menu-down"
+              ></v-btn>
+            </template>
+          </v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list
+          :lines="false"
+          density="compact"
+          nav
+          >
+          <router-link
+            v-for="(item, i) in items"
+            :key="i"
+            :to="item.to"
+            >
+            <v-list-item
+              :value="item"
+              color="primary">
+              <template v-slot:prepend>
+                <v-icon :icon="item.icon"></v-icon>
+              </template>
+
+              <v-list-item-title v-text="item.text"></v-list-item-title>         
+            </v-list-item>
+          </router-link>
+        </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Application</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
   </v-app>
 </template>
 
 <script setup>
-  import DefaultBar from './default/AppBar.vue'
-  import DefaultView from './default/View.vue'
+  import { ref } from 'vue'
+
+  const drawer = ref(null);
+
+  const items = [
+        { text: 'Home', icon: 'mdi-folder', to: "/" },
+        { text: 'About', icon: 'mdi-account-multiple', to: "/about" },
+      ];
+
 </script>
